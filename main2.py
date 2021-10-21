105d7000900a35986cb36feb1b87c797855f7a0a
Max <42574081+User53352@users.noreply.github.com>
2021-10-21 23:48:12 +0300

    Update main2.py
    cookie & requests

--- main2.py
+++ main2.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, current_app, redirect
 
 app = Flask('__name__')
 
@@ -14,6 +14,30 @@ def user_profile(user_id):
 @app.route('/books/<genre>/')
 def books(genre):
     return 'Все книги в категории "<u>{}</u>"'.format(genre)
+res = make_response("All Books in {} category".format(genre))
+    res.headers['Content-Type'] = 'text/plain'
+    res.headers['Server'] = 'Foobar'
+    return res
+
+@app.route('/') #возвращаем ошибку 404
+def http_404_handler():
+    return make_response("<h2>404 Error</h2>", 400)
+
+@app.route('/set-cookie')# устанавливаем два куки в клиентском браузере
+def set_cookie():
+    res = make_response("Cookie setter")
+    res.set_cookie("favorite-color", "skyblue")
+    res.set_cookie("favorite-font", "sans-serif")
+    return res
+@app.route('/') #указываем заголовки с помощью кортежей
+def render_markdown():
+    return "## Heading", 200, {'Content-Type': 'text/markdown'}
+
+@app.route('/transfer')#перенаправляем клиента с помощью redirect вместо "", 302, 
+def transfer():
+    return "", 302, {'location': 'https://localhost:5000/login'}
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
